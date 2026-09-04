7b27058afcb5754a3539ce66b0d57a94355b5677
Lakshit jain <jainlakshaya812@gmail.com>
2026-09-04 15:02:09 +0530

    Time: 97 ms (10.15%), Space: 373.8 MB (7.55%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    bool check(vector<int>&nums,vector<vector<int>>&queries,int k){
+    bool solve(vector<int>&nums,vector<vector<int>>&queries,int k){
         vector<int>diff(nums.size()+1);
         for(int i=0;i<k;i++){
             int start=queries[i][0];
             int end=queries[i][1];
             int val=queries[i][2];
-            diff[start]+=val;
-            diff[end+1]-=val;
+            diff[start]-=val;
+            diff[end+1]+=val;
         }
-        vector<int>ans(nums.size(),0);
+        vector<int> temp = nums;
         int curr=0;
         for(int i=0;i<nums.size();i++){
-            curr+=diff[i];
-            ans[i]=curr;
+            curr += diff[i];
+            temp[i]+=curr;
         }
-        for(int i=0;i<nums.size();i++){
-            if(ans[i]<nums[i]) return false;
-        }
-        return true;
+        int maxi=*max_element(temp.begin(),temp.end());
+        if(maxi<=0) return true;
+        else return false;
     }
-    int minZeroArray(vector<int>& nums,vector<vector<int>>& queries) {
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int low=0;
         int high=queries.size();
         int ans=INT_MAX;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(check(nums,queries,mid)){
-                ans=min(ans,mid);
-                 high=mid-1;
+            if(solve(nums,queries,mid)){
+                high=mid-1;
+                ans=mid;
             }
-            else low=mid+1;
+            else {
+                low=mid+1;
+            }
         }
         if(ans==INT_MAX) return -1;
         else
